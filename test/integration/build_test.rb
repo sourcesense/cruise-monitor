d130d6d662c1fb112e97aed552527a186097dd80
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2011-09-07 19:12:47 +0200

    more tests for Build

---
+++ test/integration/build_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + '/../test_helper'
+include Build
+
+class BuildTest < Test::Unit::TestCase
+  
+  STORAGE_FILE = '/tmp/latest.info'
+  
+  def test_cruise_rb
+    server = cruise_at("", STORAGE_FILE)
+    assert_parser_type(CruiseControlRbParser, server)
+  end
+  
+  def test_hudson
+    server = hudson_at("", STORAGE_FILE)
+    assert_parser_type(HudsonParser, server)
+  end
+  
+  def test_ccnet_feed_url
+    feed_url = 'http://localhost/ccnet/server/local/project/myproject/RSSFeed.aspx'
+    server = ccnet_at(feed_url, STORAGE_FILE)
+
+    assert_parser_type CruiseControlNetParser, server
+  end
+  
+  def test_ccnet_page_url
+    feed_url = 'http://localhost/ccnet/server/local/project/myproject/ViewAllBuilds.aspx'
+    server = ccnet_at(feed_url, STORAGE_FILE)
+
+    assert_parser_type CruiseControlNetHtmlParser, server
+  end
+
+private
+
+  def assert_parser_type(expected, server)
+    parser = ''
+    server.instance_eval { @feed.instance_eval { parser = @parser } }
+
+    assert_equal expected, parser.class
+  end
+  
+end
