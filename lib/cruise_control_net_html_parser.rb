fa9c20f47b0cbe65a2520017b4d40bbf952d5fa3
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2011-09-08 19:18:31 +0200

    refactored CruiseControl.NET html parser

--- lib/cruise_control_net_html_parser.rb
+++ lib/cruise_control_net_html_parser.rb
@@ -1,35 +1,52 @@
 class CruiseControlNetHtmlParser
   
-  def parse(feed)    
+  class HtmlInfo
+    
+    def initialize(content)
+      @content = content
+    end
+    
+    def build_number
+      failed? ? timestamp : number
+    end
+    
+    def status
+      as_status(Text.words_in(@content).at(2))
+    end
+    
+  private
+
+    def number
+      as_number(Text.words_in(@content).at(2))
+    end
+    
+    def timestamp
+      date = Text.words_in(@content).at(0)
+      time = Text.words_in(@content).at(1)
+
+      "#{date.gsub('-', '')}#{time.gsub(':', '')}"
+    end
+
+    def failed?
+      status == 'failed'
+    end
+    
+    def as_status(value)
+      return 'failed' if as_number(value) == 'Failed'
+      return 'success'
+    end
+
+    def as_number(value)
+      value.gsub('(', '').gsub(')', '')
+    end
+  end
+  
+  def parse(feed)
     project_name = feed.xpath("//table[@class='breadcrumbs']//a").last.text
     item_title = feed.xpath("//a[starts-with(@class,'build-')]").first.text
 
-    status = as_status(Text.words_in(item_title).at(2))
-    if(status == 'failed')
-      number = as_timestamp(item_title)
-    else
-      number = as_number(Text.words_in(item_title).at(2))
-    end
-    
-    "Cruise #{project_name} #{number} #{status}"
-  end
-
-private
-
-  def as_number(value)
-    value.gsub('(', '').gsub(')', '')
-  end
-
-  def as_status(value)
-    return 'failed' if as_number(value) == 'Failed'
-    return 'success'
-  end
-  
-  def as_timestamp(title)
-    date = Text.words_in(title).at(0)
-    time = Text.words_in(title).at(1)
-
-    "#{date.gsub('-', '')}#{time.gsub(':', '')}"
+    html_info = HtmlInfo.new(item_title)
+    "Cruise #{project_name} #{html_info.build_number} #{html_info.status}"
   end
   
 end
