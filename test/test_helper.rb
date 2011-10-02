d285773cf236085cbe69cfdc79f868672ace1864
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2011-10-02 21:41:40 +0200

    fixed class loading for base tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ require 'test/unit'
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require File.expand_path(file) }
 Dir[File.dirname(__FILE__) + '/doubles/*.rb'].each { |double| require File.expand_path(double) }
+Dir[File.dirname(__FILE__) + '/**/base_*_test.rb'].each { |double| require File.expand_path(double) }
 
 def assert_false(value)
   assert !value, 'expected to be false'
